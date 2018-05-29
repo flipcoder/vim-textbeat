fab7dab2b756515d927d225cde5d486452b5224c
Grady O'Connell <flipcoder@gmail.com>
2018-05-29 01:41:20 -0700

    follow playback

--- plugin/dc.py
+++ plugin/dc.py
@@ -1,7 +1,10 @@
 import vim,subprocess,os
+import select
 
 DECADENCE_PATH = os.path.join(os.path.expanduser('~'),'bin','decadence')
 DEVNULL = open(os.devnull, 'w')
+
+i = 0
 
 class VimDecadencePlugin:
     def __init__(self):
@@ -10,18 +13,30 @@ class VimDecadencePlugin:
     def stop(self):
         term = 0
         for p in self.processes:
-            if not p.poll():
-                p.terminate()
+            if p.poll()==None:
+                try:
+                    p.terminate()
+                except:
+                    pass
                 term += 1
+        self.processes = []
         return term
-    def playpause(self):
+    def refresh(self):
+        self.processes = filter(lambda p: p.poll()!=None, self.processes)
+    def play(self):
         if self.stop():
             return
+        vim.command('call dc#starttimer()')
+        vim.command('set cursorline')
+        print 'Playing'
         self.processes.append(subprocess.Popen([\
             DECADENCE_PATH,\
-            '+'+str(vim.current.window.cursor[0]),\
+            '+'+str(max(0,vim.current.window.cursor[0]-1)),\
             vim.current.buffer.name\
-        ], stdout=DEVNULL, stderr=DEVNULL))
+        ],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            bufsize=1, universal_newlines=True
+        ))
     def playline(self):
         self.stop()
         self.processes.append(subprocess.Popen([\
@@ -29,6 +44,34 @@ class VimDecadencePlugin:
             '-l',\
             vim.current.line\
         ], stdout=DEVNULL, stderr=DEVNULL))
+    def poll(self):
+        running = 0
+        done = False
+        active = 0
+        for p in self.processes:
+            working = False
+            if p.poll()==None:
+                active += 1
+            while True:
+                sel = None
+                try:
+                    sel = select.select([p.stdout],[],[],0)
+                    if not sel or not sel[0]:
+                        break
+                except:
+                    break
+                buf = p.stdout.read(1)
+                if not buf:
+                    break
+                if buf=='\n':
+                    vim.command('normal! '+str(buf.count('\n'))+'j')
+                working = True
+            if working:
+                running += 1
+        if not active:
+            print 'Stopped'
+            vim.command('call dc#stoptimer()')
+            vim.command('set cursorline&')
     def reload(self):
         for i in xrange(10): # first 10 lines check for header
             try:
@@ -47,5 +90,6 @@ class VimDecadencePlugin:
                                 x +',100,'+cs[0]+'),\',\')')
                             return
 
+
 VimDecadence = VimDecadencePlugin()
-# 
+
--- plugin/dc.vim
+++ plugin/dc.vim
@@ -4,8 +4,8 @@ endif
 
 execute 'pyfile '. escape(expand('<sfile>:p:h'),'\') . '/dc.py'
 
-function! dc#playpause()
-    py VimDecadence.playpause()
+function! dc#play()
+    py VimDecadence.play()
 endfunc
 function! dc#playline()
     py VimDecadence.playline()
@@ -13,11 +13,26 @@ endfunc
 function! dc#reload()
     py VimDecadence.reload()
 endfunc
+function! dc#poll(a)
+    py VimDecadence.poll()
+endfunc
+function! dc#starttimer()
+    if exists('s:dctimer')
+        call timer_stop(s:dctimer)
+    endif
+    let s:dctimer = timer_start(100, 'dc#poll', {'repeat':-1})
+endfunc
+function! dc#stoptimer()
+    call timer_stop(s:dctimer)
+    unlet s:dctimer
+endfunc
 
 au filetype dc set ve=all
-command! -nargs=0 DecadencePlayPause call dc#playpause()
+command! -nargs=0 DecadencePlay call dc#play()
 command! -nargs=0 DecadencePlayLine call dc#playline()
 command! -nargs=0 DecadenceReload call dc#reload()
+command! -nargs=0 DecadenceStartTimer call dc#starttimer()
+command! -nargs=0 DecadenceStopTimer call dc#stoptimer()
 
 "command! BufWritePost *.dc DecadenceReload
 "nmap <silent> <buffer> <cr> :DecadencePlayPause<cr>
